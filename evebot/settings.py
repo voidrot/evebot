0c3d32c03acfbad2a5b2280a902b0c3ab2ff1bc0
Buck Brady <bbrady145@gmail.com>
2025-03-26 22:08:38 -0600

    fix start.sh script

--- evebot/settings.py
+++ evebot/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DJANGO_DEBUG", default=True, cast=bool)
+DEBUG = env("DJANGO_DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = ["*"]
 
